756ee976685eceb3ae87a9572246be50a40bb8e3
Cara Lagumen <CaraLagumen@gmail.com>
2020-01-20 14:40:08 -0800

    added csp

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const helmet = require(`helmet`);
 const path = require("path");
 const mongoSanitize = require(`express-mongo-sanitize`);
 const xss = require(`xss-clean`);
+const csp = require(`express-csp-header`);
 
 const postsRoutes = require("./routes/posts");
 const userRoutes = require("./routes/user");
@@ -24,6 +25,15 @@ app.use(express.json({ limit: `10kb` })); //BODY PARSER & LIMIT DATA
 app.use(express.urlencoded({ extended: true, limit: `10kb` })); //PARSE DATA FROM URL ENCODED FORM
 app.use(mongoSanitize()); //DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
 app.use(xss()); //DATA SANITIZATION AGAINST XSS
+app.use(
+  csp({
+    //HEROKU BUG FIX?
+    policies: {
+      "default-src": [csp.NONE],
+      "img-src": [csp.SELF]
+    }
+  })
+);
 
 app.use("/images", express.static(path.join("images")));
 
