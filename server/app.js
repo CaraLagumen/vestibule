7a86c99f048e861fbd21b1b477f96b361f3a4322
Cara Lagumen <CaraLagumen@gmail.com>
2020-01-19 19:44:31 -0800

    heroku adjustments

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require(`helmet`);
 const path = require("path");
+const mongoSanitize = require(`express-mongo-sanitize`);
+const xss = require(`xss-clean`);
 
 const postsRoutes = require("./routes/posts");
 const userRoutes = require("./routes/user");
@@ -16,6 +18,9 @@ app.options(`*`, cors());
 app.use(helmet()); //SETUP SECURITY HEADERS
 app.use(express.json({ limit: `10kb` })); //BODY PARSER & LIMIT DATA
 app.use(express.urlencoded({ extended: true, limit: `10kb` })); //PARSE DATA FROM URL ENCODED FORM
+app.use(mongoSanitize()); //DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(xss()); //DATA SANITIZATION AGAINST XSS
+
 app.use("/images", express.static(path.join("images")));
 
 app.use("/api/posts", postsRoutes);
