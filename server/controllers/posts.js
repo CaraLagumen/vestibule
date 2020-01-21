477c059505ad6edc27479b9a03de014ac07cae00
Cara Lagumen <CaraLagumen@gmail.com>
2020-01-20 20:43:50 -0800

    updated imagePath

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,12 +1,11 @@
 const Post = require("../models/post");
 
 exports.createPost = async (req, res, next) => {
-  // const url = `${req.protocol}://${req.get("host")}`;
+  const url = `${req.protocol}://${req.get("host")}`;
   const post = await Post.create({
     title: req.body.title,
     content: req.body.content,
-    // imagePath: `${url}/images/${req.file.filename}`,,
-    imagePath: `/images/${req.file.filename}`,
+    imagePath: `${url}/images/${req.file.filename}`,
     creator: req.userData.userId
   });
 
@@ -60,9 +59,8 @@ exports.updatePost = async (req, res, next) => {
   let imagePath = req.body.imagePath;
 
   if (req.file) {
-    // const url = `${req.protocol}://${req.get("host")}`;
-    // imagePath = `${url}/images/${req.file.filename}`;
-    imagePath = `/images/${req.file.filename}`;
+    const url = `${req.protocol}://${req.get("host")}`;
+    imagePath = `${url}/images/${req.file.filename}`;
   }
 
   // const post = await Post.findByIdAndUpdate(
