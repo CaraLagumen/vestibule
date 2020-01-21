6340c120610fecea65ed3472221e22d39b4258dd
Cara Lagumen <CaraLagumen@gmail.com>
2020-01-20 18:21:38 -0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const helmet = require(`helmet`);
+const helmet = require("helmet");
 const path = require("path");
-const mongoSanitize = require(`express-mongo-sanitize`);
-const xss = require(`xss-clean`);
-const csp = require(`express-csp-header`);
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const csp = require("express-csp-header");
 
 const postsRoutes = require("./routes/posts");
 const userRoutes = require("./routes/user");
@@ -12,17 +12,17 @@ const userRoutes = require("./routes/user");
 const app = express();
 
 //TRUST HEROKU
-app.enable(`trust proxy`);
+app.enable("trust proxy");
 
 //SERVE FILES
-app.use("/images", express.static(path.join(__dirname, "images")));
-app.use("/", express.static(path.join(__dirname, `dist`, `vestibule`)));
+app.use("/images", express.static(path.join(__dirname, ".", "images")));
+app.use("/", express.static(path.join(__dirname, ".", "dist", "vestibule")));
 
 app.use(cors()); //ACCESS-CONTROL-ALLOW-ORIGIN
-app.options(`*`, cors());
+app.options("*", cors());
 app.use(helmet()); //SETUP SECURITY HEADERS
-app.use(express.json({ limit: `10kb` })); //BODY PARSER & LIMIT DATA
-app.use(express.urlencoded({ extended: true, limit: `10kb` })); //PARSE DATA FROM URL ENCODED FORM
+app.use(express.json({ limit: "10kb" })); //BODY PARSER & LIMIT DATA
+app.use(express.urlencoded({ extended: true, limit: "10kb" })); //PARSE DATA FROM URL ENCODED FORM
 app.use(mongoSanitize()); //DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
 app.use(xss()); //DATA SANITIZATION AGAINST XSS
 app.use(
@@ -37,7 +37,7 @@ app.use(
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "dist", "vestibule", "index.html"));
+  res.sendFile(path.join(__dirname, ".", "dist", "vestibule", "index.html"));
 });
 
 module.exports = app;
